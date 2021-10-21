d9b3db2eb1686d7f12e69287179730c004bfe89f
rajpericharla <38297192+Rajpericharla@users.noreply.github.com>
2021-10-20 20:39:51 -0500

    Create terraform.groovy

---
+++ terraform.groovy
@@ -0,0 +1,23 @@
+pipeline {
+	agent any
+	tools {
+		terraform 'terraform'
+	}
+	stages {
+		stage('GIT Checkout') {
+			steps {
+				git branch: 'main', credentialsId: 'git-hub', url: 'https://github.com/Rajpericharla/terraform'
+			}
+		}
+		stage('Terraform Init') {
+			steps {
+				sh label: '', script: 'terraform init'
+			}
+		}
+		stage('Terraform Apply') {
+			steps {
+				sh label: '', script: 'terraform apply --auto-approve'
+			}
+		}
+	}
+}
